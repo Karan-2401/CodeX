31b2c14398f3dab311340cb8790ad66da9e7d36a
karan-2401 <karan342772@gmail.com>
2025-11-03 18:45:23 +0530

    s

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -5,12 +5,13 @@ const app = express();
 const databaseConnection = require('./libs/db-connection')
 const cors = require('cors')
 const serve = require('inngest/express')
+const {inngest, functions} = require('./libs/inngest')
 
 // database connection
 databaseConnection()
 // database connection
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
-app.use("/api/inngest",serve({client:inngest, function }))
+app.use("/api/inngest",serve({client:inngest, functions }))
 app.use(express.json())
 
 
