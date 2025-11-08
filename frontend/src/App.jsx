3e57f85ab94d927c43e9777124116c52c5e10031
karan-2401 <karan342772@gmail.com>
2025-11-08 19:11:03 +0530

    code editor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,17 +11,21 @@ import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import Problem from "./pages/Problem";
 import { Toaster } from "react-hot-toast";
+import Dashboard from "./pages/Dashboard";
+import ProblemPage from "./pages/ProblemPage";
 
 function App() {
   const [count, setCount] = useState(0);
-  const {isSignedIn} = useUser()
+  const { isSignedIn } = useUser();
   return (
     <>
-    <Routes>
-      <Route path="/" element={<HomePage/>}/>
-       <Route path="/problem" element={<Problem/>}/>
-    </Routes>
-      <Toaster/>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/problems" element={<Problem />} />
+        <Route path="/problem/:id" element={<ProblemPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+      <Toaster />
     </>
   );
 }
